5f286970d5961847c53ea21f4f4934ee5ce99c49
hdbr00 <h_ledezma506_08_12@outlook.com>
2025-04-17 02:33:20 -0600

    Add Models

---
+++ WebVotingSystem.Models/Publicacion.cs
@@ -0,0 +1,21 @@
+﻿using WebVotingSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebVotingSystem.Models
+{
+    public class Publicacion
+    {
+        [Key]
+        public int IdPublicacion { get; set; }
+        public string UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+        [Required]
+        public string Titulo { get; set; }
+        [Required]
+        public string Contenido { get; set; }
+    }
+}
